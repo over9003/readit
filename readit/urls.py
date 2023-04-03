9d4b2c582d78745a2f74e498288821aff91c6b0a
Gabriel <gabrielvartolomeu@yahoo.com>
2023-04-03 17:14:20 +0000

    added new stuff

--- readit/urls.py
+++ readit/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.urls import include, re_path
 from django.contrib import admin
-from django.urls import path
+
+from books.views import list_books
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', list_books, name="books")
 ]
+
+
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
